8222518fb0771ddabb243e9b314f60dc5dc17710
HIEPPPP <HIEPPPP@gmail,com>
2023-05-04 09:38:52 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list");
+const btn = document.querySelector("button");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; //them icon x vao trong the span
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+inputBox.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        addTask();
+    }
+});
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove("li");
+        saveData();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
